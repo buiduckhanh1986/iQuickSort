889cd232357f19ae2d4adb4c405a0fb0a924e4d5
Bui Duc Khanh <buiduckhanh@hos-macbook-pro.local>
2016-08-24 22:41:23 +0700

    Initial Commit

---
+++ iQuickSort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iQuickSort
+//
+//  Created by Bui Duc Khanh on 8/24/16.
+//  Copyright © 2016 Bui Duc Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
